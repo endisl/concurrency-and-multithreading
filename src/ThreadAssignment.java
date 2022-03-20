cd7466c5a3eeacd3149eac381ff7e588ee216d54
endisl <endiluamba@gmail.com>
2022-03-20 16:47:14 -0300

    joining threads

--- src/ThreadAssignment.java
+++ src/ThreadAssignment.java
@@ -3,9 +3,15 @@ public class ThreadAssignment {
     public static void show() {
         System.out.println(Thread.currentThread().getName());
 
-        for (var i = 0; i < 10; i++) {
-            Thread thread = new Thread(new DownloadFileTask());
-            thread.start();
+        Thread thread = new Thread(new DownloadFileTask());
+        thread.start();
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
+        System.out.println("File is ready to be scanned.");
     }
 }
