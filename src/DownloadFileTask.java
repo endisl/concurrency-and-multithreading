f4e0bcacc72b2c4d67ad75a412398f08fd91909d
endisl <endiluamba@gmail.com>
2022-03-20 16:39:51 -0300

    pausing a thread

--- src/DownloadFileTask.java
+++ src/DownloadFileTask.java
@@ -3,5 +3,13 @@ public class DownloadFileTask implements Runnable {
     @Override
     public void run() {
         System.out.println("Downloading a file: " + Thread.currentThread().getName());
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Download complete: " + Thread.currentThread().getName());
     }
 }
