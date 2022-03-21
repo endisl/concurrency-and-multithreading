2a23896acdbbc872b323d369964345554560c2cf
endisl <endiluamba@gmail.com>
2022-03-21 20:26:11 -0300

    locks strategy

--- src/DownloadStatus.java
+++ src/DownloadStatus.java
@@ -1,11 +1,21 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class DownloadStatus {
     private int totalBytes;
+    private Lock lock = new ReentrantLock();
 
     public int getTotalBytes() {
         return totalBytes;
     }
 
     public void incrementTotalBytes() {
-        totalBytes++;
+        lock.lock();
+        try {
+            totalBytes++;
+        }
+        finally {
+            lock.unlock();
+        }
     }
 }
