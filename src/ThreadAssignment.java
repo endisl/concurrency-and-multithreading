726465b495949b5bff3bbf553b43af3fb64194fe
endisl <endiluamba@gmail.com>
2022-03-22 17:47:18 -0300

    concurrent collections

--- src/ThreadAssignment.java
+++ src/ThreadAssignment.java
@@ -1,23 +1,12 @@
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ThreadAssignment {
 
     public static void show() {
-        Collection<Integer> collection = Collections.synchronizedCollection(new ArrayList<>());
-
-        var thread1 = new Thread(() -> collection.addAll(Arrays.asList(1, 2, 3)));
-        var thread2 = new Thread(() -> collection.addAll(Arrays.asList(4, 5, 6)));
-
-        thread1.start();
-        thread2.start();
-
-        try {
-            thread1.join();
-            thread2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println(collection);
+        Map<Integer, String> map = new ConcurrentHashMap<>();
+        map.put(1, "a");
+        map.get(1);
+        map.remove(1);
     }
 }
