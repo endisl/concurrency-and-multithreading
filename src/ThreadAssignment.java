4063b4f0bdec20ea1ded5983f86b4dd0c2430ad7
endisl <endiluamba@gmail.com>
2022-03-22 17:41:24 -0300

    synchronized collections

--- src/ThreadAssignment.java
+++ src/ThreadAssignment.java
@@ -1,29 +1,23 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class ThreadAssignment {
 
     public static void show() {
+        Collection<Integer> collection = Collections.synchronizedCollection(new ArrayList<>());
 
-        var status = new DownloadStatus();
+        var thread1 = new Thread(() -> collection.addAll(Arrays.asList(1, 2, 3)));
+        var thread2 = new Thread(() -> collection.addAll(Arrays.asList(4, 5, 6)));
 
-        List<Thread> threads = new ArrayList<>();
+        thread1.start();
+        thread2.start();
 
-        for (var i = 0; i < 10; i++) {
-            var thread = new Thread(new DownloadFileTask(status));
-            thread.start();
-            threads.add(thread);
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
-        for (var thread : threads) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        System.out.println(status.getTotalBytes());
+        System.out.println(collection);
     }
 }
