6921b3b8f5b03d9705f4c68e4f3c2f1007f31420
endisl <endiluamba@gmail.com>
2022-03-22 14:49:17 -0300

    synchronization strategy

--- src/DownloadStatus.java
+++ src/DownloadStatus.java
@@ -1,21 +1,24 @@
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
 public class DownloadStatus {
     private int totalBytes;
-    private Lock lock = new ReentrantLock();
+    private int totalFiles;
+    private Object totalBytesLock = new Object();
+    private Object totalFilesLock = new Object();
 
     public int getTotalBytes() {
         return totalBytes;
     }
 
     public void incrementTotalBytes() {
-        lock.lock();
-        try {
+        synchronized (totalBytesLock) {
             totalBytes++;
         }
-        finally {
-            lock.unlock();
-        }
+    }
+
+    public synchronized void incrementTotalFiles() {
+        //synchronization on the current object and not on the dedicated monitoring object
+        //this approach can cause a lot of overhead
+        //as a good practice don't use synchronization in new code
+        //because the code runs sequentially and concurrency is lost
+        totalFiles++;
     }
 }
